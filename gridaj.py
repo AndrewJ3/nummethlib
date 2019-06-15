6be8097d126e813bc357921b3551e6e673356c14
AndrewJ3 <andrewjones237@u.boisestaate.edu>
2019-06-15 12:19:07 -0600

    gridaj

---
+++ gridaj.py
@@ -0,0 +1,18 @@
+def gridaj(a,b,nx,ny):
+    hx=(b-a)/(nx-1);hy=(b-a)/(ny-1)
+    x=np.zeros(nx)
+    y=np.zeros(ny)
+    gridx=np.zeros((nx)**2)
+    gridy=np.zeros((ny)**2)
+    for i in range(0,nx):
+        x[i] = i*hx -b
+        y[i] = i*hy -b  
+    for i in range(0,nx):  
+        for j in range(0,nx):
+            gridx[i*(nx)+j] = x[j]
+    
+    for i in range(0,ny):  
+        for j in range(0,ny):
+            gridy[j*(ny)+i] = y[j]
+
+    return gridx,gridy
